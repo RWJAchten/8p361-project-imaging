0e719a85fd14c41bf4f10e08a8ea60211602ffc2
JulienVermeer <j.b.a.f.vermeer@student.tue.nl>
2025-02-18 13:21:23 +0100

    Exercise3

--- code/mlp.py
+++ code/mlp.py
@@ -20,7 +20,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Flatten, Dense
 from tensorflow.keras.callbacks import TensorBoard
 
-
+"""
 # load the dataset using the builtin Keras method
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -68,6 +68,91 @@ model.add(Flatten(input_shape=(28,28,1)))
 model.add(Dense(64, activation='relu'))
 # output layer with 10 nodes (one for each class) and softmax nonlinearity
 model.add(Dense(10, activation='softmax')) 
+
+
+# compile the model
+model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+
+# use this variable to name your model
+model_name="my_first_model"
+
+# create a way to monitor our model in Tensorboard
+tensorboard = TensorBoard("logs/" + model_name)
+
+# train the model
+model.fit(X_train, y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_val, y_val), callbacks=[tensorboard])
+
+
+score = model.evaluate(X_test, y_test, verbose=0)
+
+
+print("Loss: ",score[0])
+print("Accuracy: ",score[1])
+"""
+
+
+#Exercise 3
+# load the dataset using the builtin Keras method
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+# mapping to 4 labels
+mapping = {
+        1: 0, 7: 0,  # "vertical digits" - 1, 7 receive label '0'
+        0: 1, 6: 1, 8: 1, 9: 1,  # "loopy digits" - 0, 6, 8, 9 receive label '1'
+        2: 2, 5: 2,  # "curly digits" - 2, 5 receive label '2'
+        3: 3, 4: 3   # "other" - 3, 4 receive label '3'
+    }
+
+y_train = np.array([mapping[label] for label in y_train])
+y_test = np.array([mapping[label] for label in y_test])
+
+
+# derive a validation set from the training set
+# the original training set is split into 
+# new training set (90%) and a validation set (10%)
+X_train, X_val = train_test_split(X_train, test_size=0.10, random_state=101)
+y_train, y_val = train_test_split(y_train, test_size=0.10, random_state=101)
+
+
+# the shape of the data matrix is NxHxW, where
+# N is the number of images,
+# H and W are the height and width of the images
+# keras expect the data to have shape NxHxWxC, where
+# C is the channel dimension
+X_train = np.reshape(X_train, (-1,28,28,1)) 
+X_val = np.reshape(X_val, (-1,28,28,1))
+X_test = np.reshape(X_test, (-1,28,28,1))
+
+
+# convert the datatype to float32
+X_train = X_train.astype('float32')
+X_val = X_val.astype('float32')
+X_test = X_test.astype('float32')
+
+
+# normalize our data values to the range [0,1]
+X_train /= 255
+X_val /= 255
+X_test /= 255
+
+
+# convert 1D class arrays to 4D class matrices, since we now only have 4 classes
+y_train = to_categorical(y_train, 4)
+y_val = to_categorical(y_val, 4)
+y_test = to_categorical(y_test, 4)
+
+
+model = Sequential()
+# flatten the 28x28x1 pixel input images to a row of pixels (a 1D-array)
+model.add(Flatten(input_shape=(28,28,1))) 
+# fully connected layer with 64 neurons and ReLU nonlinearity
+model.add(Dense(64, activation='relu'))
+# output layer with 32 nodes (one for each class) and softmax nonlinearity
+model.add(Dense(32, activation='relu'))
+# output layer with 16 nodes (one for each class) and softmax nonlinearity
+model.add(Dense(16, activation='relu'))
+# output layer with 4 nodes (one for each class) and softmax nonlinearity
+model.add(Dense(4, activation='softmax')) 
 
 
 # compile the model
