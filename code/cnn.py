9bc75c6d425bbf516e1051ea59789e0c18820b23
NienkeSengers2004 <n.c.e.sengers@student.tue.nl>
2025-03-09 19:46:14 +0100

    running models again in notebook

--- code/cnn.py
+++ code/cnn.py
@@ -10,6 +10,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 import tensorflow as tf
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
@@ -51,34 +52,34 @@ def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
      return train_gen, val_gen
 
 
-def get_model(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filters=64):
+def get_model_exercise_1(kernel_size=(3,3), pool_size=(4,4), first_filters=32, second_filters=64):
 
      # build the model
-     model = Sequential()
+     first_model = Sequential()
 
-     model.add(Conv2D(first_filters, kernel_size, activation = 'relu', padding = 'same', input_shape = (IMAGE_SIZE, IMAGE_SIZE, 3)))
-     model.add(MaxPool2D(pool_size = pool_size))
+     first_model.add(Conv2D(first_filters, kernel_size, activation = 'relu', padding = 'same', input_shape = (IMAGE_SIZE, IMAGE_SIZE, 3)))
+     first_model.add(MaxPool2D(pool_size = pool_size))
 
-     model.add(Conv2D(second_filters, kernel_size, activation = 'relu', padding = 'same'))
-     model.add(MaxPool2D(pool_size = pool_size))
+     first_model.add(Conv2D(second_filters, kernel_size, activation = 'relu', padding = 'same'))
+     first_model.add(MaxPool2D(pool_size = pool_size))
 
-     model.add(Flatten())
-     model.add(Dense(64, activation = 'relu'))
-     model.add(Dense(1, activation = 'sigmoid'))
+     first_model.add(Flatten())
+     first_model.add(Dense(64, activation = 'relu'))
+     first_model.add(Dense(1, activation = 'sigmoid'))
 
 
      # compile the model
-     model.compile(SGD(learning_rate=0.01, momentum=0.95), loss = 'binary_crossentropy', metrics=['accuracy'])
+     first_model.compile(SGD(learning_rate=0.01, momentum=0.95), loss = 'binary_crossentropy', metrics=['accuracy'])
 
-     return model
+     return first_model
 
 
 # get the model
-model = get_model()
+model_exercise_1 = get_model_exercise_1()
 
 
 # get the data generators
-train_gen, val_gen = get_pcam_generators('Data') # change this to the path of your data directory
+train_gen, val_gen = get_pcam_generators('C://Users//20223842//OneDrive - TU Eindhoven//Documents//2024-2025//project imaging') # change this to the path of your data directory
 
 
 
@@ -87,7 +88,7 @@ model_name = 'my_first_cnn_model'
 model_filepath = model_name + '.json'
 weights_filepath = model_name + '_weights.keras'
 
-model_json = model.to_json() # serialize model to JSON
+model_json = model_exercise_1.to_json() # serialize model to JSON
 with open(model_filepath, 'w') as json_file:
     json_file.write(model_json)
 
@@ -102,7 +103,7 @@ callbacks_list = [checkpoint, tensorboard]
 train_steps = train_gen.n//train_gen.batch_size
 val_steps = val_gen.n//val_gen.batch_size
 
-history = model.fit(train_gen, steps_per_epoch=train_steps,
+history = model_exercise_1.fit(train_gen, steps_per_epoch=train_steps,
                     validation_data=val_gen,
                     validation_steps=val_steps,
                     epochs=3,
@@ -111,3 +112,23 @@ history = model.fit(train_gen, steps_per_epoch=train_steps,
 # ROC analysis
 
 # TODO Perform ROC analysis on the validation set
+y_pred_prob = model_exercise_1.predict(val_gen)  # Returns a probability per image
+y_true = val_gen.classes  # The real labels (0 or 1) from the validation set
+
+# Determine the ROC curve
+fpr, tpr, thresholds = roc_curve(y_true, y_pred_prob)
+roc_auc = auc(fpr, tpr)
+ 
+# Plot the ROC curve
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
+print(history.history['accuracy'])
