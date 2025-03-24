4997d46e9a36f705b5ea097c8e0f16c3cacacb5d
Robin888888888 <r.w.j.achten@student.tue.nl>
2025-03-24 13:16:05 +0100

    tests

--- code/test_1conv_1vit.py
+++ code/test_1conv_1vit.py
@@ -7,7 +7,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Flatten
-from tensorflow.keras.layers import Conv2D, GlobalAveragePooling2D, GlobalAveragePooling1D, DepthwiseConv2D, MaxPooling2D
+from tensorflow.keras.layers import Conv2D, GlobalAveragePooling2D, GlobalAveragePooling1D, DepthwiseConv2D, MaxPooling2D, Conv1D
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
@@ -18,7 +18,7 @@ from tensorflow.keras.layers import MultiHeadAttention
 from tensorflow.keras.layers import Reshape
 from tensorflow.keras.layers import ReLU
 from tensorflow.keras.layers import Add
-from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers import SGD, Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 
 
@@ -52,102 +52,120 @@ def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
 #os.chdir('..')
 dir=os.getcwd()
 
-def transformer_block(x, embed_dim, num_heads=8, ff_dim=256, dropout_rate=0.1): 
+
+def transformer_block(x, channel, num_heads=8, dropout_rate=0.1): 
+
+    ff_dim=4*channel
+
     # Pre-normalization
     x = LayerNormalization(epsilon=1e-6)(x)
-
     # Multi-head attention layer
-    attn_output = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)(x, x)
+    attn_output = MultiHeadAttention(num_heads=num_heads, key_dim=channel)(x, x)
     attn_output = Dropout(dropout_rate)(attn_output) # dropout to prevent overfitting
 
     # Post-normalization
     x = LayerNormalization(epsilon=1e-6)(x + attn_output)  
-    
+ 
     # Feed-forward network (Multi Layer Perceptron)
-    ffn_output = tf.keras.Sequential([
-        Dense(ff_dim, activation="gelu"), # expands feature dimension and introduces non-linearity (to recognize complex patterns)
-        Dense(embed_dim) # projects back to original size
-    ])(x)
-    ffn_output = Dropout(dropout_rate)(ffn_output) # dropout for generalization
-
+    x = Dense(ff_dim, activation="gelu")(x), # expands feature dimension and introduces non-linearity (to recognize complex patterns)
+    x = Dropout(dropout_rate)(x) # dropout for generalization
+  
+    x = Dense(channel)(x) # projects back to original size
+    ffn_output = Dropout(dropout_rate)(x) # dropout for generalization
+  
     output=LayerNormalization(epsilon=1e-6)(x + ffn_output)
+    output=tf.squeeze(output,axis=0)
 
     return output
 
 # testing with one MBConv and one ViT layer with differing metalayers
-def inverted_residual_block(input, expand=64, squeeze=16):
-    m = Conv2D(expand, (1,1), activation=None, padding='same', use_bias=False)(input)
+def inverted_residual_block(input, indim, channel, expand=4):
 
+    # expand with expand*amount of channels and reduce dimensions by factor 2
+    m = Conv2D(filters=expand*indim[-1], kernel_size=(3,3), strides=(2,2), activation=None, padding='same')(input)
+
+    # perform depthwise convolution
     m = DepthwiseConv2D((3,3), activation=None, padding='same', use_bias=False)(m)
 
-    output = Conv2D(squeeze, (1,1), activation=None, padding='same', use_bias=False)(m)
+    #squeeze to desired amount of channels
+    output = Conv2D(channel, (1,1), activation=None, padding='same', use_bias=False)(m)
     output = BatchNormalization()(output)
 
-    # Residual Connection (Skip Connection)
-    shortcut = input
-    if input.shape[-1] != squeeze: 
-        shortcut = Conv2D(squeeze, (1,1), padding='same', use_bias=False)(input)
-    
-    output = Add()([shortcut, output])  
-    output = ReLU()(output)  
+    output = tf.nn.gelu(output)  
 
     return output
 
 # def CoAtNet(input_shape, 
-#             MBConv1_expand=64, MBConv1_squeeze=3,  
-#             num_heads1=4,
+#             channels=[64,96,192,384,768],
+#             dropout_rate=.1,
+#             num_heads=8,
 #             num_classes=1):
     
 #     inputs = Input(shape=input_shape)
-    
+
+#     # first convolution to transform 96x96x3 image into 48x48x64
+#     x = Conv2D(filters=64,kernel_size=(3,3),strides=(2,2),padding='same')(inputs)
+
 #     # 2x CNN block
-#     x = inverted_residual_block(inputs, MBConv1_expand, MBConv1_squeeze) 
+#     x = inverted_residual_block(x, x.shape, channel=channels[1]) 
+#     x = inverted_residual_block(x, x.shape, channel=channels[2])
 
-#     #x = Conv2D(8, (3,3), strides=2, padding='same', activation='relu')(x)
-#     x = MaxPooling2D(pool_size=(4,4))(x)
 
-#     # Automatic reshaping
-#     x = Reshape((-1, x.shape[-1]))(x)
+#     # reduce dimensions by factor two and get right amount of channels
+#     x = Conv2D(filters=channels[3], kernel_size=(3,3), strides=(2,2), padding='same')(x)
+#     # reshape into 1d image
+#     batch, height, width, channel = x.shape
+#     x = Reshape((height*width, channel))(x)
 
 #     # 2x transformer block
-#     x = transformer_block(x, embed_dim=x.shape[-1], num_heads=num_heads1)
+#     x = transformer_block(x, channel=channels[3], num_heads=num_heads, dropout_rate=dropout_rate)
+#     x = Conv1D(filters=channels[4], kernel_size=3, strides=2, padding='same')(x)
+#     x = transformer_block(x, channel=channels[4], num_heads=num_heads, dropout_rate=dropout_rate)
     
-#     x = GlobalAveragePooling1D()(x)
-#     outputs = Dense(num_classes, activation="sigmoid")(x)
+    
+#     #x = GlobalAveragePooling1D(x.shape[1]//32)(x)
+#     x = Dense(num_classes, activation="sigmoid")(x)
+#     outputs = Dropout(.1)(x) # dropout to prevent overfitting
+    
     
 #     return Model(inputs, outputs)
 
 def CoAtNet(input_shape, 
-            MBConv1_expand=64, MBConv1_squeeze=16, 
-            MBConv2_expand=32, MBConv2_squeeze=8, 
-            num_heads1=4, num_heads2=4, 
+             channels=[8,16,32],
+            dropout_rate=.1,
+            num_heads=4,
             num_classes=1):
     
     inputs = Input(shape=input_shape)
-    
-    # 2x CNN block
-    x = inverted_residual_block(inputs, MBConv1_expand, MBConv1_squeeze) 
-    x = inverted_residual_block(x, MBConv2_expand , MBConv2_squeeze)
 
-    x = Conv2D(32, (3,3), strides=2, padding='same', activation='relu')(x)
-    x = MaxPooling2D(pool_size=(4,4))(x)
-    
-    # Automatic reshaping
-    x = Reshape((-1, x.shape[-1]))(x)
+    # first convolution to transform 96x96x3 image into 48x48x64
+    x = Conv2D(filters=channels[0],kernel_size=(3,3),strides=(2,2),padding='same')(inputs)
+
+    # 2x CNN block
+    x = inverted_residual_block(x, x.shape, channel=channels[1]) 
+
+
+    # reduce dimensions by factor two and get right amount of channels
+    x = Conv2D(filters=channels[2], kernel_size=(3,3), strides=(2,2), padding='same')(x)
+    # reshape into 1d image
+    batch, height, width, channel = x.shape
+    x = Reshape((height*width, channel))(x)
 
     # 2x transformer block
-    x = transformer_block(x, embed_dim=x.shape[-1], num_heads=num_heads1)
-    x = transformer_block(x, embed_dim=x.shape[-1], num_heads=num_heads2)
+    x = transformer_block(x, channel=channels[2], num_heads=num_heads, dropout_rate=dropout_rate)
+
     
-    x = GlobalAveragePooling1D()(x)
-    outputs = Dense(num_classes, activation="sigmoid")(x)
+    #x = GlobalAveragePooling1D(x.shape[1]//32)(x)
+    x = Dense(num_classes, activation="sigmoid")(x)
+    outputs = Dropout(.1)(x) # dropout to prevent overfitting
+    
     
     return Model(inputs, outputs)
 
 input_shape = (IMAGE_SIZE, IMAGE_SIZE, 3)
 model = CoAtNet(input_shape)
-model.compile(SGD(learning_rate=0.001, momentum=0.95), loss = 'binary_crossentropy', metrics=['accuracy'])
-model_name = 'CoAtNet_2MBConv_4x4pool_2ViT_10epochs'
+model.compile(Adam(learning_rate=0.1), loss = 'binary_crossentropy', metrics=['accuracy'])
+model_name = 'CoAtNet0'
 
 
 # save the model and weights
@@ -174,22 +192,30 @@ callbacks_list = [checkpoint, tensorboard]
 train_steps = train_gen.n//train_gen.batch_size
 val_steps = val_gen.n//val_gen.batch_size
 
-#os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
 history = model.fit(train_gen, steps_per_epoch=train_steps,
                     validation_data=val_gen,
                     validation_steps=val_steps,
-                    epochs=50,
+                    epochs=10,
                     callbacks=callbacks_list)
 
 from sklearn.metrics import accuracy_score, recall_score
-y_pred_prob = model.predict(val_gen)  # Returns a probability per image
-# Zet de voorspelde waarschijnlijkheden om naar binaire labels (0 of 1)
-y_pred = (y_pred_prob > 0.5).astype(int)  # Binaire voorspelling op basis van 0.5 drempel
-
-y_true = val_gen.classes  # The real labels (0 or 1) from the validation set
+y_pred_prob = list(model.predict(val_gen).flatten())  # Returns a probability per image
+y_pred=[1 if num >= .5 else 0 for num in y_pred_prob]
+y_true = list(val_gen.classes)  # The real labels (0 or 1) from the validation set
 
 accuracy = accuracy_score(y_true, y_pred)
 recall = recall_score(y_true, y_pred, average='binary') 
 
 print(f'Accuracy: {accuracy:.4f}')
 print(f'Recall: {recall:.4f}')
+
+from sklearn.metrics import roc_curve, auc
+fpr, tpr, thresholds = roc_curve(y_true, y_pred_prob)
+    
+# Compute the AUC
+roc_auc = auc(fpr, tpr)
+print(roc_auc)
+
+from sklearn.metrics import confusion_matrix as cm
+cm(y_pred,y_true)
